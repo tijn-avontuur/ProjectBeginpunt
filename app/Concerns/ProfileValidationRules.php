4fce87854c83844b0433b33134f8b86337f5d548
tijn-avontuur <bananenvla1234@gmail.com>
2026-01-30 14:27:34 +0100

    Fix Pest errors

--- app/Concerns/ProfileValidationRules.php
+++ app/Concerns/ProfileValidationRules.php
@@ -43,8 +43,8 @@ trait ProfileValidationRules
             'email',
             'max:255',
             $userId === null
-                ? Rule::unique(User::class)
-                : Rule::unique(User::class)->ignore($userId),
+                ? Rule::unique('users')
+                : Rule::unique('users')->ignore($userId),
         ];
     }
 }
---
+++ app/Models/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * Get the user's initials.
+     */
+    public function initials(): string
+    {
+        $names = explode(' ', $this->name);
+
+        if (count($names) === 1) {
+            return strtoupper(substr($names[0], 0, 2));
+        }
+
+        return strtoupper(substr($names[0], 0, 1) . substr(end($names), 0, 1));
+    }
+}
